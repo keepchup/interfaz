6467c3405e08265b5c34e0ce8e2f365695f96f2e
Luis Adrian Ramos <150483323+keepchup@users.noreply.github.com>
2024-06-20 09:54:10 -0500

    Add files via upload

--- interfazGrafica/src/Main.java
+++ interfazGrafica/src/Main.java
@@ -1,5 +1,16 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hola mundo");
 
+        String num1 = JOptionPane.showInputDialog("Ingrese un numero");
+        String num2 = JOptionPane.showInputDialog("Ingrese un numero");
+
+        int n1=Integer.parseInt(num1);
+        int n2=Integer.parseInt(num2);
+        int sum=n1*n2;
+
+        JOptionPane.showMessageDialog(null, "El resultado es: "+sum, "Suma de numeros enteros", JOptionPane.PLAIN_MESSAGE);
     }
 }
---
+++ interfazGrafica/src/form1.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+
+public class form1 {
+    private JPanel panel1;
+    private JButton button1;
+}
